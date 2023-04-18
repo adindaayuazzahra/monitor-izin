ec790fd3a43075f0694730e4079de8ddc9e5d19c
Adinda Ayu Azzahra <4519210056@univpancasila.ac.id>
2023-04-18 22:36:35 +0700

    commit benerin tampilan awal

--- resources/views/admin/perijinan.blade.php
+++ resources/views/admin/perijinan.blade.php
@@ -1,13 +1,13 @@
 @extends('index')
 @section('content')
     {{-- <section style="height:250px;background-color:#0288F6;">
-        <div class="align-items-start justify-content-center d-flex pt-5">
-            <div class="text-white text-center ">
-                <h1 style="font-size:26pt;">Mengelola Perijinan</h1>
-                <p style="font-size:16pt;">Pada halaman ini anda dapat menambahkan, menyunting dan menghpaus perizinan</p>
-            </div>
+    <div class="align-items-start justify-content-center d-flex pt-5">
+        <div class="text-white text-center ">
+            <h1 style="font-size:26pt;">Mengelola Perijinan</h1>
+            <p style="font-size:16pt;">Pada halaman ini anda dapat menambahkan, menyunting dan menghpaus perizinan</p>
         </div>
-    </section> --}}
+    </div>
+</section> --}}
     <div class="hero-container" style="background-image: url({{ asset('assets/img-jmtm/gambar1.jpg') }})">
         <div class="overlay" style="opacity: 0.5"></div>
         <div class="container align-items-center justify-content-center d-flex" style="padding-top: 90px; ">
@@ -25,14 +25,14 @@
                 <i class="fas fa-search text-white" style="font-size: 12pt;"></i>
                 <h5 style="margin-left: 10px;">Daftar Perijinan</h5>
                 {{-- <div class="d-flex">
-                    <i class="fas fa-search text-white" style="font-size: 12pt;"></i>
-                    <h5 style="margin-left: 10px;">Daftar Perijinan</h5>
-                </div>
-                <div class="">
-                    <a href="" type="button" class="btn px-3  text-white" style="background: #873FFD;border-radius:30px;">
-                        <i class="fa-solid fa-folder-plus" style="margin-right: 5px"></i>
-                    </a>
-                </div> --}}
+                <i class="fas fa-search text-white" style="font-size: 12pt;"></i>
+                <h5 style="margin-left: 10px;">Daftar Perijinan</h5>
+            </div>
+            <div class="">
+                <a href="" type="button" class="btn px-3  text-white" style="background: #873FFD;border-radius:30px;">
+                    <i class="fa-solid fa-folder-plus" style="margin-right: 5px"></i>
+                </a>
+            </div> --}}
             </div>
             <div class="card" style="margin:10px 30px 30px 30px; background-color: transparent;border: none;">
                 <div class="d-flex justify-content-md-end justify-content-xs-center">
@@ -63,16 +63,13 @@
                         @foreach ($perijinans as $p)
                             <tr
                                 class="
-                                    @if (!$p->tanggal_berakhir)''
-                                        @if($p->status == 1)
-                                            bg-danger
-                                        @endif
+                                    @if (!$p->tanggal_berakhir) ''
+                                        @if ($p->status == 1)
+                                            bg-danger @endif
                                     @else
-                                        @if ($p->status == 1) 
-                                            bg-danger
+                                        @if ($p->status == 1) bg-danger
                                         @else
-                                            {{ Carbon::now()->startOfDay()->diffInMonths($p->tanggal_berakhir, false) <= 3? 'bg-warning': '' }} 
-                                        @endif
+                                            {{ Carbon::now()->startOfDay()->diffInMonths($p->tanggal_berakhir, false) <= 3? 'bg-warning': '' }} @endif 
                                     @endif
                                 ">
                                 <td>{{ $i }}</td> @php $i++ @endphp
@@ -81,17 +78,21 @@
                                 </td>
                                 <td>
                                     @if (!$p->tanggal_berakhir)
-                                        <p>-</p>
+                                        @if ($p->status_perpanjangan == 0)
+                                            <p>Selama Perusahaan Menjalankan Usaha</p>
+                                        @else
+                                            <p>-</p>
+                                        @endif
                                     @else
-                                    {{ Carbon::make($p->tanggal_berakhir)->format('d/m/Y') ?? '-' }}
+                                        {{ Carbon::make($p->tanggal_berakhir)->format('d/m/Y') ?? '-' }}
                                     @endif
                                 </td>
                                 <td>
                                     {{ $p['instansi_terkait'] }}
                                 </td>
                                 {{-- <td>
-                                    {{ $p['perkiraan_proses'] }}
-                                </td> --}}
+                            {{ $p['perkiraan_proses'] }}
+                        </td> --}}
                                 <td>
                                     @if ($p->status == 0)
                                         <span class="rounded-pill badge text-bg-success">Aktif</span>
@@ -100,8 +101,9 @@
                                     @endif
                                 </td>
                                 {{-- <td>
-                                    <a href="" class="btn btn-danger" >Lihat Detail <i class="fa-solid fa-up-right-from-square"></i></a>
-                                </td> --}}
+                            <a href="" class="btn btn-danger">Lihat Detail <i
+                                    class="fa-solid fa-up-right-from-square"></i></a>
+                        </td> --}}
                                 <td>
                                     <a href="{{ route('admin.perijinan.detail', ['id' => $p->id]) }}" class="detail">Lihat
                                         Detail <i class="fa-solid fa-up-right-from-square"></i></i></a>
